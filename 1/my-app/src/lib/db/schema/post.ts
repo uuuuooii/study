5bae96fb0cb6ed7cd83fca0f8b6e7ca220c26d2f
neo@zillinks.com <keunhey0107@gmail.com>
2023-11-07 20:48:39 +0900

    Update: mongoose 연결

---
+++ 1/my-app/src/lib/db/schema/post.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const postSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+export default mongoose.model('Post', postSchema);
