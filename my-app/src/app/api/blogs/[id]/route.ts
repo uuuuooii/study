687dd95822ffccc2c81a224d5a08e76efd88880d
neo@zillinks.com <keunhey0107@gmail.com>
2023-12-11 20:40:28 +0900

    Update: 다이나믹라우팅 api제작

--- my-app/src/app/api/blogs/[id]/route.ts
+++ my-app/src/app/api/blogs/[id]/route.ts
@@ -7,6 +7,7 @@ export const GET = async (
   request: NextRequest,
   { params }: { params: { id: string } }
 ) => {
+  // 비록 함수 본문에서 명시적으로 사용되지 않을지라도 이는 함수 서명의 중요한 부분이며 Next.js API 라우트에서 필요한 부분입니다.
   const { id } = params;
   try {
     await connect();
